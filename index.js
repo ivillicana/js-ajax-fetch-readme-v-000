814a5e32f56c77fa5581710c0631c0b101202fb8
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-08-22 11:15:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+const token = '8dc9b6e9f0e472f1e61cba392eeea9d4c2d6149b';
+fetch('https://api.github.com/ivillicana/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
